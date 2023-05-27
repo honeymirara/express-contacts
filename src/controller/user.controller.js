9fd250d05a9baf075ca87138246617ce047cc2c7
honeymirara <honeymirara@gmail.com>
2023-05-28 02:37:55 +0300

    buildResponse

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const { isValidEnvironmentId, isValidBody } = require('../helper/validation');
 const { getAllData, getById, createData, updateDataDB, deleteDataByIdDB } = require('../service/user.service');
+const { buildResponse } = require("../helper/buildResponse");
 const route = express.Router();
 
 route.get(`/`, async (req, res) => {
@@ -7,48 +9,48 @@ route.get(`/`, async (req, res) => {
         const data = await getAllData();
         res.send(data);
     } catch (err) {
-        res.send(err.message);
+        buildResponse(res, 404, err.message);
     }
 });
 
-route.get('/:id', async (req, res) => {
+route.get('/:id', isValidEnvironmentId, async (req, res) => {
     try {
         const { id } = req.params;
         const data = await getById(id);
         res.send(data);
     } catch (err) {
-        res.send(err.message);
+        buildResponse(res, 404, err.message);
     }
 });
 
-route.post('/', async (req, res) => {
+route.post('/', isValidEnvironmentId, isValidBody, async (req, res) => {
     try {
         const { name, surname, birth, city, age } = req.body;
         const data = await createData(name, surname, birth, city, age);
         res.send(data);
     } catch (err) {
-        res.send(err.message);
+        buildResponse(res, 404, err.message);
     }
 });
 
-route.put('/:id', async (req, res) => {
+route.put('/:id', isValidEnvironmentId, isValidBody, async (req, res) => {
     try {
         const { id } = req.params;
         const { name, surname, birth, city, age } = req.body;
         const data = await updateDataDB(id, name, surname, birth, city, age);
         res.send(data);
     } catch (err) {
-        res.send(err);
+        buildResponse(res, 404, err.message);
     }
 });
 
-route.delete('/:id', async (req, res) => {
+route.delete('/:id', isValidEnvironmentId, async (req, res) => {
     try {
         const { id } = req.params;
         const data = await deleteDataByIdDB(id)
         res.send(data);
     } catch (err) {
-        res.send(err);
+        buildResponse(res, 404, err.message);
     }
 })
 
